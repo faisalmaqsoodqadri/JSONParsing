795e2de69871b59e3cb812e6617e8d916a06e28e
Faisal Maqsood <fmqswift@yahoo.com>
2021-03-12 08:02:41 +0200

    Initial Commit

---
+++ JSONDataDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JSONDataDemo
+//
+//  Created by Faisal Maqsood Qadri on 12/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
